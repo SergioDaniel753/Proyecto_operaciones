46966ee84cc589ca2789735f6e25b9fddab5fb04
Sergio Daniel Quevedo <a2019114381@estudiantes.upsa.edu.bo>
2021-01-15 18:16:47 -0400

    Resta Get y Post

--- Solución_operaciones/Web_OperacionesN/Controllers/CalculatorController.cs
+++ Solución_operaciones/Web_OperacionesN/Controllers/CalculatorController.cs
@@ -11,15 +11,32 @@ namespace Web_OperacionesN.Controllers
     public class CalculatorController : Controller
     {
         [HttpGet]
+        [Route("suma")]
         public int suma(int num1, int num2)
         {
             return num1 + num2;
         }
         [HttpPost]
+        [Route("suma")]
         public int sumar([FromHeader] int num1, [FromHeader] int num2)
         {
             return num1 + num2;
         }
+
+        [HttpGet]
+        [Route("resta")]
+        public int resta(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        [HttpPost]
+        [Route("resta")]
+        public int restar([FromHeader] int num1, [FromHeader] int num2)
+        {
+            return num1 - num2;
+        }
+
+        
 
     }
 }
